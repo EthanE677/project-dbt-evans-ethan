b0295270ed0593a430ad6808bc273cdb36a99fc9
EthanE677 <eva21020@byui.edu>
2026-03-23 14:22:21 -0600

    .

--- itm327_evans_ethan/models/staging/raw_news.sql
+++ itm327_evans_ethan/models/staging/raw_news.sql
@@ -1,3 +1,3 @@
 -- TODO: Update the source table name to match your prefix (e.g., SMITHJ_NEWS)
 select *
-from {{ source('snowbearair', 'LASTN_FI_NEWS') }}
+from {{ source('snowbearair', 'SFTP_API_EVANS_E') }}
